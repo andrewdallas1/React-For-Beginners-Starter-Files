ea913268780c047c00cfb59ec707a57c6944da4e
andrew dallas <adal1@live.com>
2017-01-02 17:23:15 -0500

    finished video 27

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -2,14 +2,16 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { BrowserRouter, Match, Miss} from 'react-router';
+
 import './css/style.css';
 import App from './components/App';
 import StorePicker from './components/StorePicker';
 import NotFound from './components/NotFound';
 
+const repo = `/${window.location.pathname.split('/')[1]}`
 const Root = () => {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/fish-app/">
       <div>
         <Match exactly pattern="/" component={StorePicker} />
         <Match pattern="/store/:storeId" component={App} />
